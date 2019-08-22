b80e8fda4a6494ac1e03e66ec0c114ed85473cb2
Phillip Kruger <phillip.kruger@gmail.com>
2019-08-22 13:32:56 +0200

    Token for SOAP Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- jello-cards/src/main/java/com/github/phillipkruger/jello/api/CardSoapApi.java
+++ jello-cards/src/main/java/com/github/phillipkruger/jello/api/CardSoapApi.java
@@ -2,6 +2,8 @@ package com.github.phillipkruger.jello.api;
 
 import com.github.phillipkruger.jello.Card;
 import com.github.phillipkruger.jello.service.CardService;
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -11,6 +13,7 @@ import javax.jws.WebService;
  * Basic SOAP API for Cards
  * @author Phillip Kruger (phillip.kruger@redhat.com)
  */
+@DeclareRoles({"admin","user"})
 @WebService
 public class CardSoapApi implements CardApi {
 
@@ -19,12 +22,14 @@ public class CardSoapApi implements CardApi {
     
     @WebMethod
     @Override
+    @RolesAllowed("user")
     public Card createCard(@WebParam Card card){
         return cardService.createCard(card);
     }
     
     @WebMethod
     @Override
+    @RolesAllowed("user")
     public Card updateCard(@WebParam Card card){
         if(card.getId()==null)return createCard(card);
         return cardService.updateCard(card);
@@ -32,12 +37,14 @@ public class CardSoapApi implements CardApi {
     
     @WebMethod
     @Override
+    @RolesAllowed("user")
     public Card getCard(@WebParam Long id){
         return cardService.getCard(id);
     }
     
     @WebMethod
     @Override
+    @RolesAllowed("admin")
     public void removeCard(@WebParam Long id){
         Card card = getCard(id);
         if(card!=null)cardService.removeCard(card);
